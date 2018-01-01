17dd84765eeb903645374e714afaba07024e6e4c
First Last <salmanashraf853@gmail.com>
2018-01-01 20:13:44 +0100

    finishing up

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,14 +3,9 @@ import './footer.css'
 
 const Footer = () => {
     return (
-        <div className="section footer">
+        <div className="footer">
             <div className="section_content">
-                <p className="footer_text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna 
-                    aliqua. 
-                </p>
-                <p className="copy">&copy; All rights reserved by ashrofdev</p>
+                <p className="copy">&copy; All rights reserved by <a style={{color:'lightGreen'}} href="https://ashrof.herokuapp.com/">ashrofDev</a></p>
             </div>
         </div>
     );
