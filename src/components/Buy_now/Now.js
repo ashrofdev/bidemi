42450f931e802cc21e690ccba6b824003f37dc51
First Last <salmanashraf853@gmail.com>
2020-01-12 12:43:16 +0100

    improving routing ui

--- src/components/Buy_now/Now.js
+++ src/components/Buy_now/Now.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import './now.css'
 
+import Fade from 'react-reveal/Fade'
+
 const Now = ({onRouteChange}) => {
     return (
         <div className="buy_now">
-            <div className="content">
-                <h3>Fill the form below</h3>
-                <p>All fields are required</p>
-                <div className="form">
-                    <input placeholder="Lastname"/>
-                    <input placeholder="Firstname"/>
-                    <input placeholder="Email"/>
-                    <input placeholder="Phone no"/>
-                    <input placeholder="Delivery address"/>
+            <Fade bottom>
+                <div className="content">
+                    <h3>Fill the form below</h3>
+                    <p>All fields are required</p>
+                    <div className="form">
+                        <input placeholder="Lastname"/>
+                        <input placeholder="Firstname"/>
+                        <input placeholder="Email"/>
+                        <input placeholder="Phone no"/>
+                        <input placeholder="Delivery address"/>
+                    </div>
+                    <div className="cta">
+                        <button onClick={()=>onRouteChange('details')} className="back">CANCEL</button>
+                        <button>CONFIRM</button>
+                    </div>
                 </div>
-                <div className="cta">
-                    <button onClick={()=>onRouteChange('details')} className="back">CANCEL</button>
-                    <button>CONFIRM</button>
-                </div>
-            </div>
+            </Fade>
         </div>
     );
 };
