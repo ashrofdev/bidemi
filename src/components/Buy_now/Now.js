1d8f16f8ed89881515df5a70fd746807922d243c
First Last <salmanashraf853@gmail.com>
2020-01-12 12:34:35 +0100

    improving routing ui

--- src/components/Buy_now/Now.js
+++ src/components/Buy_now/Now.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './now.css'
 
-const Now = () => {
+const Now = ({onRouteChange}) => {
     return (
         <div className="buy_now">
             <div className="content">
                 <h3>Fill the form below</h3>
-                <p>All filds are required</p>
+                <p>All fields are required</p>
                 <div className="form">
                     <input placeholder="Lastname"/>
                     <input placeholder="Firstname"/>
                     <input placeholder="Email"/>
                     <input placeholder="Phone no"/>
                     <input placeholder="Delivery address"/>
+                </div>
+                <div className="cta">
+                    <button onClick={()=>onRouteChange('details')} className="back">CANCEL</button>
+                    <button>CONFIRM</button>
                 </div>
             </div>
         </div>
