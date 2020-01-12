7e1ed182f1514bb4e0b348657cbade3bbadcf598
First Last <salmanashraf853@gmail.com>
2020-01-12 11:33:41 +0100

    building buy now functionality

---
+++ src/components/Buy_now/Now.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Now = () => {
+    return (
+        <div className="form">
+            <input placeholder="Lastname"/>
+            <input placeholder="Firstname"/>
+            <input placeholder="Email"/>
+            <input placeholder="Phone no"/>
+            <input placeholder="Lastname"/>
+        </div>
+    );
+};
+
+export default Now;
