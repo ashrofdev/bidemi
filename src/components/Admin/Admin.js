c8cefc79891fdd7f887d4abca988a2106757b664
First Last <salmanashraf853@gmail.com>
2020-01-24 12:19:58 +0100

    starting the admin page

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade'
 
 class Admin extends Component {
     render() {
         return (
-            <div className="admin">
-                ADMIN PAGE
-            </div>
+            <Fade>
+                <div className="admin">
+                    ADMIN PAGE
+                </div>
+            </Fade>
         );
     }
 }
