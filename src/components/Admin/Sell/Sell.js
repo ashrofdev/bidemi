4d26a7b4915856f06e0179222a2eb9ebf3423a3d
First Last <salmanashraf853@gmail.com>
2020-02-11 14:49:00 +0100

    styling the admin page

--- src/components/Admin/Sell/Sell.js
+++ src/components/Admin/Sell/Sell.js
@@ -7,14 +7,16 @@ const Sell = ({sell, upload, back}) => {
         <Slide left>
              <div className="sell">
                 <input onChange={(e)=>upload(e.target.files[0])} type="file"/>
-                <input className="sell_name" placeholder="Product name"/>
-                <input className="sell_memory" placeholder="SSD memory size"/>
-                <input className="sell_ram" placeholder="RAM size"/>
-                <input className="sell_core" placeholder="core"/>
-                <input className="sell_width" placeholder="screen size"/>
-                <input className="sell_brand" placeholder="manufacturer"/>
-                <input className="sell_price" placeholder="Prduct price"/>
-                <button onClick={sell}>SELL</button>
+                <div className="fields">
+                    <input className="sell_name" placeholder="Product name"/>
+                    <input className="sell_memory" placeholder="SSD memory size"/>
+                    <input className="sell_ram" placeholder="RAM size"/>
+                    <input className="sell_core" placeholder="core"/>
+                    <input className="sell_width" placeholder="screen size"/>
+                    <input className="sell_brand" placeholder="manufacturer"/>
+                    <input type="number" className="sell_price" placeholder="Prduct price"/>
+                </div>
+                <button className="sell" onClick={sell}>SELL</button>
                 <button onClick={()=>back('admin')} className="a_back">BACK</button>
             </div>
         </Slide>
