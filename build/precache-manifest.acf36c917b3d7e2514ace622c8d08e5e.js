1a53b26d637a1e674a79e1fd9b3e36142c9feb28
First Last <salmanashraf853@gmail.com>
2020-02-12 15:36:49 +0100

    changing title

--- build/precache-manifest.acf36c917b3d7e2514ace622c8d08e5e.js
+++ build/precache-manifest.acf36c917b3d7e2514ace622c8d08e5e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1be10ea6599cdaf7492ca92fbffa58d0",
+    "revision": "18815546eacd352750f78051d90f5a88",
     "url": "/bidemi/index.html"
   },
   {
-    "revision": "230ede166779973f6b3f",
-    "url": "/bidemi/static/css/main.6a5d57d6.chunk.css"
+    "revision": "7d07df0021947b7956b6",
+    "url": "/bidemi/static/css/main.1bf7dd4e.chunk.css"
   },
   {
-    "revision": "e78a5ad4333131240c54",
-    "url": "/bidemi/static/js/2.73e7ea3f.chunk.js"
+    "revision": "c6d7201b944e0f669fc1",
+    "url": "/bidemi/static/js/2.5e6bb258.chunk.js"
   },
   {
-    "revision": "230ede166779973f6b3f",
-    "url": "/bidemi/static/js/main.436f0f65.chunk.js"
+    "revision": "7d07df0021947b7956b6",
+    "url": "/bidemi/static/js/main.55149196.chunk.js"
   },
   {
     "revision": "b278bbdbf0ba6f4d7abc",
@@ -34,9 +34,5 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "efd6830c763671e0c6893836b2a9dd7d",
     "url": "/bidemi/static/media/mac.efd6830c.jpg"
-  },
-  {
-    "revision": "4c3bb8153387301e6a8920994f6f93ea",
-    "url": "/bidemi/static/media/samsung.4c3bb815.jpg"
   }
 ]);
